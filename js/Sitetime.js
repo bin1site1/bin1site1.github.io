45699526ed59ab2f9a371ce7db5f18b5f755ac6b
bin1 <daihualianghuabin@gmail.com>
2024-07-01 21:31:49 +0800

    更新 Sitetime.js

--- js/Sitetime.js
+++ js/Sitetime.js
@@ -1,11 +1,9 @@
 // 运行时间
 function siteTime() {
+  // 设置定时器，每秒调用一次 siteTime 函数
   window.setTimeout(siteTime, 1000);
-  var seconds = 1000;
-  var minutes = seconds * 60;
-  var hours = minutes * 60;
-  var days = hours * 24;
-  var years = days * 365;
+
+  // 获取当前日期和时间
   var today = new Date();
   var todayYear = today.getFullYear();
   var todayMonth = today.getMonth() + 1;
@@ -14,14 +12,24 @@ function siteTime() {
   var todayMinute = today.getMinutes();
   var todaySecond = today.getSeconds();
 
-  var t1 = Date.UTC(2022, 0, 7, 0, 0, 0); // 注意月份是从 0 开始的
-  var t2 = Date.UTC(todayYear, todayMonth - 1, todayDate, todayHour, todayMinute, todaySecond); // 注意月份是从 0 开始的
+  // 计算特定日期的时间戳（注意月份是从 0 开始的）
+  var t1 = Date.UTC(2022, 1, 7, 0, 0, 0);
+  // 计算当前日期的时间戳（注意月份是从 0 开始的）
+  var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
+  // 计算两个时间戳的差值
   var diff = t2 - t1;
-  var diffYears = Math.floor(diff / years);
-  var diffDays = Math.floor((diff / days) - diffYears * 365);
-  var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
-  var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
-  var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds);
+
+  // 计算天数差值
+  var diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+  // 计算小时差值
+  var diffHours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  // 计算分钟差值
+  var diffMinutes = Math.floor((diff / (1000 * 60)) % 60);
+  // 计算秒数差值
+  var diffSeconds = Math.floor(diff / 1000) % 60;
+
+  // 更新页面上的运行时间显示
   document.getElementById("sitetime").innerHTML = "本站已运行 " + diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
 }
+
 siteTime();
